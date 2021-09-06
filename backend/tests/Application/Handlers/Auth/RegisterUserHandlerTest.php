84e3907e314fe314451f308e3d94e160c09bc875
David Alonso <davidealonso.da@gmail.com>
2021-09-05 23:30:42 -0300

    Wip: testing

---
+++ backend/tests/Application/Handlers/Auth/RegisterUserHandlerTest.php
@@ -0,0 +1,17 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace tests\Application\Handlers\Auth;
+
+use PHPUnit\Framework\TestCase;
+
+final class RegisterUserHandlerTest extends TestCase
+{
+    public function testCanRegisterValidUser(): void
+    {
+        $this->assertTrue(true);
+    }
+}
+
+
